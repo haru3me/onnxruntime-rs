b4b6419c615ba3b2700013f3f0339b2bd5116684
haru3me <121174745+haru3me@users.noreply.github.com>
2022-12-22 23:05:48 +0900

    型修正3

--- onnxruntime/src/lib.rs
+++ onnxruntime/src/lib.rs
@@ -190,10 +190,18 @@ fn g_ort() -> sys::OrtApi {
     unsafe { *api_ptr_mut }
 }
 
+#[cfg(not(all(target_os = "linux", target_arch = "aarch64")))]
 fn char_p_to_string(raw: *const i8) -> Result<String> {
-    #[cfg(not(all(target_os = "linux", target_arch = "aarch64")))]
     let c_string = unsafe { std::ffi::CStr::from_ptr(raw as *mut i8).to_owned() };
-    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+
+    match c_string.into_string() {
+        Ok(string) => Ok(string),
+        Err(e) => Err(OrtApiError::IntoStringError(e)),
+    }
+    .map_err(OrtError::StringConversion)
+}
+#[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+fn char_p_to_string(raw: *const u8) -> Result<String> {
     let c_string = unsafe { std::ffi::CStr::from_ptr(raw as *mut u8).to_owned() };
 
     match c_string.into_string() {
